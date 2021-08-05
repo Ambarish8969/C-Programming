46e72013d7de2d5b74d666e671d48b10a7177553
Ambarish <ambarish8653@gmail.com>
2021-08-05 17:35:38 +0530

    updated.

---
+++ input.c
@@ -0,0 +1,17 @@
+// #include<stdio.h>
+// int main()
+// {int n;
+//   printf("Enter and Interger : " );
+//   scanf("%d",&n);
+//   printf("%d\n", n);
+// }
+// ...................................................................
+#include<stdio.h>
+int main()
+{
+  int var1,var2;
+  var1=45465;
+  var2=4545;
+  printf("%d \n%d\n",var1 ,var2);
+  printf("%d\n",sizeof(int) ); // it will print the size of the Interger.
+}
---
+++ operations.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  a=20;
+  b=6;
+  printf("%d\n",a+b ); //addition
+  printf("%d\n",a-b ); //subtraction
+  printf("%d\n",a*b ); //multiplication
+  printf("%d\n",a/b ); //division
+}
