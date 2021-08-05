d02844d90784c9a5526112c6423609cc8467d51f
Ambarish <ambarish8653@gmail.com>
2021-08-05 18:08:59 +0530

    updated.

---
+++ odd_or_Even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("Enter an Integer : ");
+  scanf("%d",&a );
+  if(a%2==0){
+    printf("%d is an Even number.\n", a);
+  }
+  else{
+    printf("%d id an Odd number.\n", a);
+  }
+}
