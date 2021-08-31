451278fb7b54db487902bff409ac8635cd73c70d
Ambarish <ambarish8653@gmail.com>
2021-08-31 22:12:30 +0530

    updated

--- try.cpp
+++ try.cpp
@@ -1,15 +1,9 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main()
 {
-  int row,col;
-  cin>> row>>col;
-  for(int i=1;i<=row;i++){
-    for(int j=1;j<=col;j++){
-      if(j==2 && j==col)
-      {
-        std::cout <<;
-      }
-    }std::cout  << '\n';
-  }
+  int n;
+  cin>>n;
+  
 }
