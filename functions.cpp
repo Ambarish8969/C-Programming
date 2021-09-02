fa6996889a281ae38c04a2d22cb481039538a3d3
Ambarish <ambarish8653@gmail.com>
2021-09-02 11:24:56 +0530

    Updated.

--- functions.cpp
+++ functions.cpp
@@ -1,19 +1,33 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-int add(int a,int b){
+int add(int a,int b){ //Addtion....
   int c=a+b;
   return c;
 }
-int isprime(int num){
+int isprime(int num){   // Check Prime or Not....
   for(int i=2;i<=sqrt(num);i++){
     if(num%2==0){
       return false;
     }
   }return true;
 }
+int fib(int number){ //Fibonaccie Series....
+  int t1=0;
+  int t2=1;
+  int nextTerm;
+  for(int j=1;j<=number;j++){
+    cout <<t1<<endl;
+    nextTerm=t1+t2;
+    t1=t2;
+    t2=nextTerm;
+  }
+  return t1;
+}
 int main()
-{
-  std::cout << "The sum is : "<< add(2,3) << '\n';
-  std::cout << isprime(11) << '\n';
+{ int n;
+  cin >> n;
+  cout << "The sum is : "<< add(2,3) << '\n';
+  cout << isprime(n) << '\n';
+  cout << fib(n);
 }
