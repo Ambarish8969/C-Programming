477d09cbdee641e174fc1f3cd46d33ae8e50bf50
Ambarish <ambarish8653@gmail.com>
2021-09-24 18:31:02 +0530

    Updated.

---
+++ cmake-build-debug/CMakeFiles/C_Programming.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Programming.dir/main.cpp.obj"
+  "C_Programming.exe"
+  "C_Programming.exe.manifest"
+  "C_Programming.pdb"
+  "libC_Programming.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_Programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
